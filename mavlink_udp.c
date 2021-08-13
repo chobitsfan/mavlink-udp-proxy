a23566737de90b3d07ab2efc26ff7432d574fb15
chobits <chobits@itri.org.tw>
2021-08-13 14:09:58 +0800

    uart ok

---
+++ mavlink_udp.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <termios.h> // Contains POSIX terminal control definitions
+#include <errno.h> // Error integer and strerror() function
+#include "ardupilotmega/mavlink.h"
+
+bool gogogo = true;
+
+void sig_handler(int signum) {
+    gogogo = false;
+}
+
+int main(void) {
+    fd_set rfds;
+    struct timeval tv;
+    int retval, uart_fd;
+    char buf[512];
+    ssize_t avail;
+    mavlink_status_t status;
+    mavlink_message_t msg;
+    // Create new termios struc, we call it 'tty' for convention
+    struct termios tty;
+
+    signal(SIGINT, sig_handler);
+
+    uart_fd = open("/dev/ttyMSM1", O_RDWR);
+
+    if(tcgetattr(uart_fd, &tty) != 0) {
+        printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+        return 1;
+    }
+    tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+    tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+    tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size 
+    tty.c_cflag |= CS8; // 8 bits per byte (most common)
+    tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+    tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+    tty.c_lflag &= ~ICANON;
+    tty.c_lflag &= ~ECHO; // Disable echo
+    tty.c_lflag &= ~ECHOE; // Disable erasure
+    tty.c_lflag &= ~ECHONL; // Disable new-line echo
+    tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+    tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+    tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+    tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+    tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+    tty.c_cc[VMIN] = 0;
+    cfsetispeed(&tty, B115200);
+    cfsetospeed(&tty, B115200);
+    // Save tty settings, also checking for error
+    if (tcsetattr(uart_fd, TCSANOW, &tty) != 0) {
+        printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+        return 1;
+    }
+
+    printf("hello\n");
+
+    while (gogogo) {
+        FD_ZERO(&rfds);
+        FD_SET(uart_fd, &rfds);
+
+        tv.tv_sec = 10;
+        tv.tv_usec = 0;
+
+        retval = select(uart_fd + 1, &rfds, NULL, NULL, &tv);
+        if (retval > 0) {
+            avail = read(uart_fd, buf, 512);
+            printf("recv %d bytes\n", avail);
+            for (int i = 0; i < avail; i++) {
+                if (mavlink_parse_char(0, buf[i], &msg, &status)) {
+                    printf("recv msg ID %d, seq %d\n", msg.msgid, msg.seq);
+                }
+            }
+        }
+    }
+
+    return 0;
+}
