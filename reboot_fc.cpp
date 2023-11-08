8af446bbfac9b57996aa27ce04c30160074124ad
chobits <chobits@itri.org.tw>
2023-11-08 17:26:59 +0800

    reboot fc tool

---
+++ reboot_fc.cpp
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <termios.h> // Contains POSIX terminal control definitions
+#include <errno.h> // Error integer and strerror() function
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <math.h>
+#include <sys/wait.h>
+#include <poll.h>
+#include <time.h>
+#include "mavlink/ardupilotmega/mavlink.h"
+
+#define MY_COMP_ID 191
+#define MY_NUM_PFDS 3
+
+int main(int argc, char *argv[]) {
+    struct pollfd pfds[MY_NUM_PFDS];
+    int retval, uart_fd;
+    unsigned int len;
+    unsigned char buf[512];
+    ssize_t avail;
+    mavlink_status_t status;
+    mavlink_message_t msg;
+    // Create new termios struc, we call it 'tty' for convention
+    struct termios tty;
+    uint8_t mav_sysid = 0;
+
+    if (argc > 1)
+        uart_fd = open(argv[1], O_RDWR| O_NOCTTY);
+    else
+        uart_fd = open("/dev/ttyTHS0", O_RDWR | O_NOCTTY);
+    if (uart_fd < 0) {
+        printf("can not open serial port\n");
+        return 1;
+    }
+
+    if(tcgetattr(uart_fd, &tty) != 0) {
+        printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+        return 1;
+    }
+    tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+    tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+    tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size
+    tty.c_cflag |= CS8; // 8 bits per byte (most common)
+    tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+    tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+    tty.c_lflag &= ~ICANON;
+    tty.c_lflag &= ~ECHO; // Disable echo
+    tty.c_lflag &= ~ECHOE; // Disable erasure
+    tty.c_lflag &= ~ECHONL; // Disable new-line echo
+    tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+    tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+    tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+    tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+    tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+    tty.c_cc[VMIN] = 0;
+    cfsetispeed(&tty, B921600);
+    cfsetospeed(&tty, B921600);
+    // Save tty settings, also checking for error
+    if (tcsetattr(uart_fd, TCSANOW, &tty) != 0) {
+        printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+        return 1;
+    }
+
+    pfds[0].fd= uart_fd;
+    pfds[0].events = POLLIN;
+
+    printf("hello\n");
+
+    bool gogogo = true;
+    while (gogogo) {
+        retval = poll(pfds, MY_NUM_PFDS, 5000);
+        if (retval > 0) {
+            if (pfds[0].revents & POLLIN) {
+                avail = read(uart_fd, buf, 512);
+                for (int i = 0; i < avail; i++) {
+                    if (mavlink_parse_char(0, buf[i], &msg, &status)) {
+                        if (msg.sysid == 255) continue;
+                        //printf("recv msg ID %d, seq %d\n", msg.msgid, msg.seq);
+                        if (msg.msgid == MAVLINK_MSG_ID_HEARTBEAT) {
+                            mavlink_heartbeat_t hb;
+                            mavlink_msg_heartbeat_decode(&msg, &hb);
+                            if (msg.sysid != mav_sysid) {
+                                mav_sysid = msg.sysid;
+                                printf("found MAV %d, reboot it\n", msg.sysid);
+                                mavlink_msg_command_long_pack(mav_sysid, MY_COMP_ID, &msg, 0, 0, MAV_CMD_PREFLIGHT_REBOOT_SHUTDOWN, 0, 1, 0, 0, 0, 0, 0, 0);
+                                len = mavlink_msg_to_send_buffer(buf, &msg);
+                                write(uart_fd, buf, len);
+                                gogogo = false;
+                            }
+                        } else if (msg.msgid == MAVLINK_MSG_ID_STATUSTEXT) {
+                            mavlink_statustext_t txt;
+                            mavlink_msg_statustext_decode(&msg, &txt);
+                            printf("fc: %s\n", txt.text);
+                        }
+                    }
+                }
+            }
+        } else break;
+    }
+
+    close(uart_fd);
+
+    printf("bye\n");
+
+    return 0;
+}
