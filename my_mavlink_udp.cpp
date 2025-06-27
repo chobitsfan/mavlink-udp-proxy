6a032377695d1f959347ccf158ae72c6cfc3a2e3
chobits tai <chobits@itri.org.tw>
2025-06-27 11:18:20 +0800

    pub more status

--- my_mavlink_udp.cpp
+++ my_mavlink_udp.cpp
@@ -63,9 +63,11 @@ class MavRosNode : public rclcpp::Node {
     public:
         MavRosNode(int uart_fd) : Node("mavlink_ros"), uart_fd_(uart_fd) {
             roll_pub = this->create_publisher<std_msgs::msg::Float32>("roll", rclcpp::QoS(1).best_effort().durability_volatile());
+            voltage_pub = this->create_publisher<std_msgs::msg::Float32>("voltage", rclcpp::QoS(1).best_effort().durability_volatile());
             sonar_pub = this->create_publisher<sensor_msgs::msg::Range>("sonar", rclcpp::QoS(1).best_effort().durability_volatile());
             vel_pub = this->create_publisher<geometry_msgs::msg::TwistStamped>("tgt_vel", rclcpp::QoS(1).best_effort().durability_volatile());
             intersect_type_pub = this->create_publisher<std_msgs::msg::Int32>("intersect_type", 1);
+            is_armable_sub = this->create_publisher<std_msgs::msg::Int32>("is_armable", 0);
             odom_sub = this->create_subscription<nav_msgs::msg::Odometry>("odometry", rclcpp::QoS(1).best_effort().durability_volatile(), [this](const nav_msgs::msg::Odometry::SharedPtr msg) { odom_callback(msg); });
             intersec_sub = this->create_subscription<geometry_msgs::msg::Point>("templateCOG", 1, [this](const geometry_msgs::msg::Point::SharedPtr msg) { intersect_callback(msg); });
             hori_line_sub = this->create_subscription<geometry_msgs::msg::Polygon>("hori_line", 1, [this](const geometry_msgs::msg::Polygon::SharedPtr msg) { hori_line_callback(msg); });
@@ -347,6 +349,16 @@ class MavRosNode : public rclcpp::Node {
                             len = mavlink_msg_to_send_buffer(buf, &msg);
                             write(uart_fd_, buf, len);
                         }
+                        if (hb.system_status == MAV_STATE_STANDBY) {
+                            // if is armable = 1
+                            auto m = std_msgs::msg::Int32();
+                            m.data = 1;
+                            is_armable_sub->publish(m);
+                        } else {
+                            auto m = std_msgs::msg::Int32();
+                            m.data = 0;
+                            is_armable_sub->publish(m);
+                        }
                     } else if (msg.msgid == MAVLINK_MSG_ID_STATUSTEXT) {
                         mavlink_statustext_t txt;
                         mavlink_msg_statustext_decode(&msg, &txt);
@@ -373,6 +385,13 @@ class MavRosNode : public rclcpp::Node {
                         rng.max_range = 7.5;
                         rng.range = dist_sensor.current_distance * 0.01;
                         sonar_pub->publish(rng);
+                    } else if (msg.msgid == MAVLINK_MSG_ID_BATTERY_STATUS) {
+                        std::cout << "check";
+                        mavlink_battery_status_t batt;
+                        mavlink_msg_battery_status_decode(&msg, &batt);
+                        auto m = std_msgs::msg::Float32();
+                        m.data = batt.voltages[0] / 1000.0;
+                        voltage_pub->publish(m);
                     }
                 }
             }
@@ -407,9 +426,11 @@ class MavRosNode : public rclcpp::Node {
         struct timeval tv_vert_line = {0, 0};
         float vert_line_p1u = 0;
         rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr roll_pub;
+        rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr voltage_pub;
         rclcpp::Publisher<sensor_msgs::msg::Range>::SharedPtr sonar_pub;
         rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr vel_pub;
         rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr intersect_type_pub;
+        rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr is_armable_sub;
         rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr intersec_sub;
         rclcpp::Subscription<geometry_msgs::msg::Polygon>::SharedPtr hori_line_sub;
         rclcpp::Subscription<geometry_msgs::msg::Polygon>::SharedPtr vert_line_sub;
